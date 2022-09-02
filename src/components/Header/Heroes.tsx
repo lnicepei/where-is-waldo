997542b72987769c8b5220de9b66dfe1a06fa268
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-02 19:31:40 +0300

    Add data about heroes

---
+++ src/components/Header/Heroes.tsx
@@ -0,0 +1,22 @@
+const Heroes = [
+  {
+    name: "Imposter",
+    found: false,
+    image:
+      "https://cdn.iconscout.com/icon/free/png-256/blue-among-us-3218506-2691064.png",
+  },
+  {
+    name: "Keanu",
+    found: false,
+    image:
+      "https://www.pngall.com/wp-content/uploads/4/Cyberpunk-2077-Keanu-Reeves-Transparent.png",
+  },
+  {
+    name: "Steve",
+    found: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png",
+  },
+];
+
+export default Heroes;
