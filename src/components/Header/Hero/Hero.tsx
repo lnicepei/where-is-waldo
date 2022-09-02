aa73114ae6b5353eceb4f4d29fc9059589d2ce9d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-02 19:34:17 +0300

    Change opacity of found heroes

--- src/components/Header/Hero/Hero.tsx
+++ src/components/Header/Hero/Hero.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import "./Hero.css";
 
 interface HeroProps {
   name: string;
   image: string;
+  found: boolean;
 }
 
 const Hero = (props: HeroProps) => {
   return (
     <div className="hero">
-      <h2>{props.name}</h2>
-      <img src={props.image} alt={props.name} />
+      {!props.found && <h2 className="hero--name">{props.name}</h2>}
+      {!props.found && (
+        <img className="hero--img" src={props.image} alt={props.name} />
+      )}
+      {props.found && <h2 className="hero--name found">{props.name}</h2>}
+      {props.found && (
+        <img className="hero--img found" src={props.image} alt={props.name} />
+      )}
     </div>
   );
 };
