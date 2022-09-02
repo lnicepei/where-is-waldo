b2697b91571b6dd94f56073813b759073fffee43
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-02 17:56:31 +0300

    Move server-side functionality to Game component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,28 +1,6 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 import "./App.css";
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
 import Game from "../components/Game/Game";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBty4ic-Qsr_wyXC_CK2XHAnxve7jE1Ysw",
-  authDomain: "where-is-waldo-bee31.firebaseapp.com",
-  projectId: "where-is-waldo-bee31",
-  storageBucket: "where-is-waldo-bee31.appspot.com",
-  messagingSenderId: "750759008904",
-  appId: "1:750759008904:web:3ddab790d89d56c29e9d3d",
-};
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Crosshair from "../Crosshair/Crosshair";
 import SearchImage from "../SearchImage/SearchImage";
+import Header from "../Header/Header";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/app";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBty4ic-Qsr_wyXC_CK2XHAnxve7jE1Ysw",
+  authDomain: "where-is-waldo-bee31.firebaseapp.com",
+  projectId: "where-is-waldo-bee31",
+  storageBucket: "where-is-waldo-bee31.appspot.com",
+  messagingSenderId: "750759008904",
+  appId: "1:750759008904:web:3ddab790d89d56c29e9d3d",
+};
 
 const Game = () => {
   const [coordinateX, setCoordinateX] = useState(0);
   const [coordinateY, setCoordinateY] = useState(0);
   const [wasClicked, setWasClicked] = useState(false);
 
+  const [rightCoordinates, setRightCoordinates] = useState<
+    { name: string; position: string }[]
+  >([]);
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth(app);
+  const db = firebase.firestore(app);
+  const positionRef = db.collection("positinOfCharacters");
+  const [position] = useCollectionData(positionRef, { idField: "id" });
+
+  useEffect(() => {
+    setRightCoordinates(position);
+  }, [position]);
+
   return (
-    <div className="App">
+    <div className="Game">
+      <Header />
       <SearchImage
         setCoordinateX={setCoordinateX}
         setCoordinateY={setCoordinateY}
@@ -19,10 +49,11 @@ const Game = () => {
           coordinateX={coordinateX}
           coordinateY={coordinateY}
           setWasClicked={setWasClicked}
+          rightCoordinates={rightCoordinates}
         />
       )}
     </div>
   );
 };
 
-export default Game ;
+export default Game;
