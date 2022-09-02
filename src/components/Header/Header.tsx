b6ef4c307e180abfcc48c71b724a4d72c6e384b2
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-02 19:33:57 +0300

    Move heroes data to a separate component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,17 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../Game/Game";
 import "./Header.css";
 import Hero from "./Hero/Hero";
+// import Heroes from "./Heroes";
 
-interface HeaderProps {}
+const Header = () => {
+  const { heroes } = useContext(AppContext);
 
-const Header = (props: HeaderProps) => {
   return (
     <header>
-      <Hero
-        name="Imposter"
-        image="https://cdn.iconscout.com/icon/free/png-256/blue-among-us-3218506-2691064.png"
-      />
-      <Hero
-        name="Keanu"
-        image="https://www.pngall.com/wp-content/uploads/4/Cyberpunk-2077-Keanu-Reeves-Transparent.png"
-      />
-      <Hero
-        name="Steve"
-        image="https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png"
-      />
+      {heroes.map((hero) => (
+        <Hero name={hero.name} image={hero.image} found={hero.found} />
+      ))}
     </header>
   );
 };
