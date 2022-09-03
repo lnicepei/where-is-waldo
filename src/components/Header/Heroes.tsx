946adc29f4ea29761c83492bd6b75c0e233f72d0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-03 16:20:02 +0300

    Change path to images to relative

--- src/components/Header/Heroes.tsx
+++ src/components/Header/Heroes.tsx
@@ -3,19 +3,18 @@ const Heroes = [
     name: "Imposter",
     found: false,
     image:
-      "https://cdn.iconscout.com/icon/free/png-256/blue-among-us-3218506-2691064.png",
+      "src/components/Header/HeroImages/blue-among-us-3218506-2691064.webp",
   },
   {
     name: "Keanu",
     found: false,
     image:
-      "https://www.pngall.com/wp-content/uploads/4/Cyberpunk-2077-Keanu-Reeves-Transparent.png",
+      "src/components/Header/HeroImages/Cyberpunk-2077-Keanu-Reeves-Transparent.png",
   },
   {
     name: "Steve",
     found: false,
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png",
+    image: "src/components/Header/HeroImages/Steve_(Minecraft).png",
   },
 ];
 
