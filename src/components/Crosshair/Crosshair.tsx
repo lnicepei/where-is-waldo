dc6a066366da6a31872b89ab6b0bbcc9cb35a5ad
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-03 17:43:26 +0300

    Remove old comments, specify search area

--- src/components/Crosshair/Crosshair.tsx
+++ src/components/Crosshair/Crosshair.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AppContext } from "../Game/Game";
 import "./Crosshair.css";
+import CrosshairButton from "./CrosshairButton/CrosshairButton";
 
 interface CrosshairProps {
   coordinateX: number;
@@ -9,30 +10,31 @@ interface CrosshairProps {
   setWasClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface Character {
+  name: string;
+  position: string;
+}
+
 const Crosshair = (props: CrosshairProps) => {
-  const crosshairSyle = {
+  const crosshairStyle = {
     left: (props.coordinateX / window.innerWidth) * 100 + "%",
     top: (props.coordinateY / window.innerHeight) * 100 + "%",
   };
-  // const crosshairSyle = {
-  //   left: props.coordinateX + "px",
-  //   top: props.coordinateY + "px",
-  // };
 
-  // const centerStyle = {
-  //   width: "50px",
-  //   height: "50px",
-  // };
-
-  const { setHeroes } = useContext(AppContext);
+  const { heroes, setHeroes } = useContext(AppContext);
 
   const handleChoiceClick = (name: string) => {
-    console.log(props.coordinateX, props.coordinateY);
     props.rightCoordinates.forEach((character) => {
-      if (character[name]) {
+      if (character[name as keyof Character]) {
         if (
-          Math.abs(props.coordinateX - +character[name].split(" ")[0]) <= 25 &&
-          Math.abs(props.coordinateY - +character[name].split(" ")[1]) <= 25
+          Math.abs(
+            (props.coordinateX / window.innerWidth) * 100 -
+              +character[name as keyof Character].split(" ")[0]
+          ) <= 1 &&
+          Math.abs(
+            (props.coordinateY / window.innerHeight) * 100 -
+              +character[name as keyof Character].split(" ")[1]
+          ) <= 2.5
         ) {
           setHeroes((prevHeroes) => {
             return prevHeroes.map((hero) => {
@@ -49,27 +51,17 @@ const Crosshair = (props: CrosshairProps) => {
   };
 
   return (
-    <div className="crosshair" style={crosshairSyle}>
-      <div className="crosshair--center"></div>
+    <div className="crosshair" style={crosshairStyle}>
       <div className="options">
-        <button
-          onClick={() => handleChoiceClick("Imposter")}
-          className="option option-1"
-        >
-          Imposter
-        </button>
-        <button
-          onClick={() => handleChoiceClick("Keanu")}
-          className="option option-2"
-        >
-          Keanu
-        </button>
-        <button
-          onClick={() => handleChoiceClick("Steve")}
-          className="option option-3"
-        >
-          Steve
-        </button>
+        {heroes.map((hero) => {
+          return (
+            <CrosshairButton
+              name={hero.name}
+              found={hero.found}
+              handleChoiceClick={handleChoiceClick}
+            />
+          );
+        })}
       </div>
     </div>
   );
