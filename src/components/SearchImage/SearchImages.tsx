aaf6d8f3d311305fdfe4c3aeb2645f25cb9b8640
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-04 19:01:52 +0300

    Add data about search images and heroes within

--- src/components/SearchImage/SearchImages.tsx
+++ src/components/SearchImage/SearchImages.tsx
@@ -1,19 +1,89 @@
 const searchImages = [
-  {
-    name: "deskmat1",
-    url: "./SearchImages/pierre-roussel-laptop-deksmat1.png",
-  },
+  // {
+  //   name: "deskmat1",
+  //   url: "src/components/SearchImage/SearchImages/pierre-roussel-laptop-deksmat1.png",
+  //   heroes: [
+  //     {
+  //       name: "Imposter",
+  //       found: false,
+  //       image:
+  //         "src/components/Header/HeroImages/blue-among-us-3218506-2691064.webp",
+  //     },
+  //     {
+  //       name: "Keanu",
+  //       found: false,
+  //       image:
+  //         "src/components/Header/HeroImages/Cyberpunk-2077-Keanu-Reeves-Transparent.png",
+  //     },
+  //     {
+  //       name: "Steve",
+  //       found: false,
+  //       image: "src/components/Header/HeroImages/Steve_(Minecraft).png",
+  //     },
+  //   ],
+  // },
   {
     name: "snes",
-    url: "./SearchImages/pierre-roussel-snes-phone2-pal.jpg",
+    url: "src/components/SearchImage/SearchImages/pierre-roussel-snes-phone2-pal.jpg",
+    heroes: [
+      {
+        name: "Red Ranger",
+        found: false,
+        image: "src/components/Header/HeroImages/red-ranger-power.png",
+      },
+      {
+        name: "Donatello",
+        found: false,
+        image: "src/components/Header/HeroImages/Donnyboy.webp",
+      },
+      {
+        name: "Bogyy B",
+        found: false,
+        image: "src/components/Header/HeroImages/bogyy-b.png",
+      },
+    ],
   },
   {
-    name: "ps3",
-    url: "./SearchImages/pierre-roussel-ps3-phone1.jpg",
+    name: "wii",
+    url: "src/components/SearchImage/SearchImages/wii.jpg",
+    heroes: [
+      {
+        name: "Bowser",
+        found: false,
+        image: "src/components/Header/HeroImages/Bowser.png",
+      },
+      {
+        name: "Luigi",
+        found: false,
+        image: "src/components/Header/HeroImages/Luigi.webp",
+      },
+      {
+        name: "Rabbid",
+        found: false,
+        image: "src/components/Header/HeroImages/Rabbid.webp",
+      },
+    ],
   },
   {
-    name: "xbox360s",
-    url: "./SearchImages/pierre-roussel-xbox360s-phone2.jpg",
+    name: "ps2",
+    url: "src/components/SearchImage/SearchImages/ps2.jpg",
+    heroes: [
+      {
+        name: "Okami",
+        found: false,
+        image: "src/components/Header/HeroImages/okami-wolf.webp",
+      },
+      {
+        name: "Legolas",
+        found: false,
+        image: "src/components/Header/HeroImages/Legolas.webp",
+      },
+      {
+        name: "Kratos",
+        found: false,
+        image: "src/components/Header/HeroImages/kratos.png",
+      },
+    ],
   },
 ];
 
