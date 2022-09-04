ff3842ea40cdf403e0593092d85731be21b9928a
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-04 10:56:03 +0300

    Create choice menu component

---
+++ src/components/SearchImage/SearchImageChoiceMenu/SearchImageChoiceMenu.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import searchImages from "../SearchImages";
+import SearchImageChoiceCard from "./SearchImageChoiceCard";
+
+interface SearchImageChoiceMenuProps {
+  className?: string;
+}
+
+const SearchImageChoiceMenu: React.FC<SearchImageChoiceMenuProps> = (props) => {
+  return (
+    <>
+      {searchImages.map((image) => {
+        return <SearchImageChoiceCard name={image.name} url={image.url} />;
+      })}
+    </>
+  );
+};
+
+export default SearchImageChoiceMenu;
