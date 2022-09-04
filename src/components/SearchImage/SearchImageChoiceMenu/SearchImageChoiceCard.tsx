78780e5c6aa8046837b8df9bc59b2da6e4b1fbe4
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-04 11:25:09 +0300

    Comment what the code does

--- src/components/SearchImage/SearchImageChoiceMenu/SearchImageChoiceCard.tsx
+++ src/components/SearchImage/SearchImageChoiceMenu/SearchImageChoiceCard.tsx
@@ -10,7 +10,7 @@ const SearchImageChoiceCard: React.FC<SearchImageChoiceCardProps> = (props) => {
   const { setWasImageChosen } = useContext(AppContext);
 
   const handleChoiceClick = () => {
-    
+    // TODO: choose current game image on this button click
     setWasImageChosen((prevWasImageChosen) => !prevWasImageChosen);
   };
 
@@ -18,7 +18,7 @@ const SearchImageChoiceCard: React.FC<SearchImageChoiceCardProps> = (props) => {
     <div className="card">
       <h1>{props.name}</h1>
       <img src={props.url} alt={props.name} />
-      <button onClick={handleChoiceClick}>Chose</button>
+      <button onClick={handleChoiceClick}>Choose</button>
     </div>
   );
 };
