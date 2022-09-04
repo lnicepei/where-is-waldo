87cde8e6a3425c2d1c9bb24d14051f60f24ee417
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-04 10:58:16 +0300

    Add style to search containers

--- src/components/SearchImage/SearchImage.style.tsx
+++ src/components/SearchImage/SearchImage.style.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import SearchImageChoiceMenu from "./SearchImageChoiceMenu/SearchImageChoiceMenu";
 
 export const StyledSearchImage = styled.img`
   width: 100vw;
@@ -9,3 +10,23 @@ export const StyledSearchContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const StyledSearchImageChoiceMenu = styled(SearchImageChoiceMenu)`
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+  width: 50px;
+  height: 50px;
+  margin-top: 200px;
+`;
+
+export const StyledSearchImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// export const StyledSearchImageChoiceMenu = styled.div`
+//   display: flex;
+//   background-color: red;
+// `
