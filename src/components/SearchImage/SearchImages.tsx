5936e5593bd765c7a63cd6732f3e3af21ff76a35
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-04 11:00:25 +0300

    Update images paths

---
+++ src/components/SearchImage/SearchImages.tsx
@@ -0,0 +1,20 @@
+const searchImages = [
+  {
+    name: "deskmat1",
+    url: "./SearchImages/pierre-roussel-laptop-deksmat1.png",
+  },
+  {
+    name: "snes",
+    url: "./SearchImages/pierre-roussel-snes-phone2-pal.jpg",
+  },
+  {
+    name: "ps3",
+    url: "./SearchImages/pierre-roussel-ps3-phone1.jpg",
+  },
+  {
+    name: "xbox360s",
+    url: "./SearchImages/pierre-roussel-xbox360s-phone2.jpg",
+  },
+];
+
+export default searchImages;
