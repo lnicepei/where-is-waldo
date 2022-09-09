00fe22821ab075d030f9acac64b66c106b230125
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-09 20:57:05 +0300

    Update timer value after image choice

--- src/components/SearchImage/SearchImageChoiceMenu/SearchImageChoiceCard.tsx
+++ src/components/SearchImage/SearchImageChoiceMenu/SearchImageChoiceCard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useAppDispatch } from "../../../App/hooks";
-import { HeroInterface } from "../../Header/Hero/Hero";
-import { setHeroes } from "../../Game/GameSlice";
 import { StyledChooseCard, StyledChooseImage } from "../SearchImage.style";
+
 import {
   setCurrentSearchImage,
   setCurrentSearchImageURL,
 } from "../SearchImageSlice";
+import { setIsCounting, setTime } from "../../../Timer/TimerSlice";
+import { useAppDispatch } from "../../../App/hooks";
+import { HeroInterface } from "../../Header/Hero/Hero";
+import { setHeroes } from "../../Game/GameSlice";
 
 interface SearchImageChoiceCardProps {
   url: string;
@@ -25,6 +27,8 @@ const SearchImageChoiceCard: React.FC<SearchImageChoiceCardProps> = (props) => {
         })
       )
     );
+    dispatch(setIsCounting(true));
+    dispatch(setTime(0));
     dispatch(setCurrentSearchImage(props.name));
     dispatch(setCurrentSearchImageURL(props.url));
   };
