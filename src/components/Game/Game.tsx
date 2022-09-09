83fa13db14fcc1962e1e6e2dbbaf51c9c775c7c1
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-09 15:42:32 +0300

    Move configuring store to SearchImage component

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,39 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import SearchImage from "../SearchImage/SearchImage";
 import Header from "../Header/Header";
 
-import { StyledGame } from "./Game.styles";
+import { store } from "../../store";
 
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-
-import heroesReducer from "./GameSlice";
-import SearchImageReducer from "../SearchImage/SearchImageSlice";
-
-export interface HeroInterface {
-  name: string;
-  found: boolean;
-  image: string;
-}
-
-const Game = () => {
-  const store = configureStore({
-    reducer: {
-      currentSearchImage: SearchImageReducer,
-      heroes: heroesReducer,
-    },
-  });
-
-  console.log("game component rendered");
-
+const Game: React.FC = () => {
   return (
-    <StyledGame>
+    <div className="Game">
       <Provider store={store}>
         <Header />
         <SearchImage />
       </Provider>
-    </StyledGame>
+    </div>
   );
 };
 
