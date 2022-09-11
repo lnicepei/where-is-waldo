0da8e8c7b63feaeed9b7e9104e8d621370e2ba23
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-11 11:19:53 +0300

    Change Time type to Date, format time

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,10 +1,12 @@
+import { format, millisecondsToMinutes } from "date-fns";
+import { millisecondsToSeconds } from "date-fns/esm";
 import React from "react";
 import { useAppSelector } from "../../App/hooks";
 
 export interface User {
   id?: string;
-  name?: string;
-  time?: string;
+  name: string;
+  time: Date;
 }
 
 const Leaderboard = () => {
@@ -16,7 +18,7 @@ const Leaderboard = () => {
     <div>
       {leaderboardData.map((user) => (
         <div>
-          {user.name} : {user.time}
+          {user.name} : {format(user.time, "mm:ss:SS")}
         </div>
       ))}
     </div>
