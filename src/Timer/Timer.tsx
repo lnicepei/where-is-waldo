da093004d5d43974938a6ce1158fd0bcab8a8747
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-11 11:22:11 +0300

    Update time each ms, format using date-fns

--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,33 +1,24 @@
 import React, { useEffect } from "react";
 import { setTime } from "./TimerSlice";
 import { useAppDispatch, useAppSelector } from "../App/hooks";
+import { addMilliseconds, differenceInMilliseconds, format } from "date-fns";
 
 const Timer = () => {
   const time = useAppSelector((state) => state.time.time);
+
   const isCounting = useAppSelector((state) => state.time.isCounting);
+  
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (isCounting) {
-      setTimeout(() => {
-        dispatch(setTime(time + 15));
-      }, 10);
-    }
+    setTimeout(() => dispatch(setTime(addMilliseconds(time, 1))), 1);
   });
 
   return (
     <>
       {isCounting && (
         <div>
-          <span className="digits">
-            {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-          </span>
-          <span className="digits">
-            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
-          </span>
-          <span className="digits mili-sec">
-            {("0" + ((time / 15) % 100)).slice(-2)}
-          </span>
+          {format(differenceInMilliseconds(new Date(), time), "mm:ss:SS")}
         </div>
       )}
     </>
