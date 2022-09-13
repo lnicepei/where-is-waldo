448f2a49b467b8b556103dd5a8a1405fa7dcd36e
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-13 09:42:02 +0300

    Show loader when loading is going

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,25 +1,32 @@
-// import React from "react";
+import React from "react";
 import { format } from "date-fns";
+import { BarLoader } from "react-spinners";
 import { useAppSelector } from "../../App/hooks";
 
 export interface User {
-  id: string;
+  id?: string;
   name?: string;
   time?: Date | number;
 }
 
 const Leaderboard = () => {
-  const leaderboardData = useAppSelector(
-    (state) => state.leaderboard.leaderboardData
+  const currentLeaderboardData = useAppSelector(
+    (state) => state.leaderboard.currentLeaderboardData
   );
+
+  console.log(currentLeaderboardData);
 
   return (
     <div>
-      {leaderboardData.map((user) => (
-        <div>
-          {user.name} : {user.time ? format(user.time, "mm:ss:SS") : ""}
-        </div>
-      ))}
+      {currentLeaderboardData.map((user) =>
+        user.name ? (
+          <div>
+            {user.name} : {user.time ? format(user.time, "mm:ss:SS") : ""}
+          </div>
+        ) : (
+          <BarLoader />
+        )
+      )}
     </div>
   );
 };
