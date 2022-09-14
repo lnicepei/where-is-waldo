1bc8cdcce35fce9cf7605a656655e57014a14772
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-14 07:54:04 +0300

    Add Global style from styled components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
+import { GlobalStyle } from "./App/App.style";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
