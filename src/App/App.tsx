8f60eac9619924a1896ec6720df099a66f6e2b84
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-14 07:54:57 +0300

    Move Header to SearchImage, remove .css import

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "../store";
-import Header from "../components/Header/Header";
 import SearchImage from "../components/SearchImage/SearchImage";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Header />
         <SearchImage />
       </Provider>
     </div>
