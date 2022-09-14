8f25accbda4c3a8bb64654cbee458969d2f9d520
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-14 07:58:49 +0300

    Load leaderboard from database

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { format } from "date-fns";
 import { BarLoader } from "react-spinners";
-import { useAppSelector } from "../../App/hooks";
+import { useAppDispatch, useAppSelector } from "../../App/hooks";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  setAllLeaderboardData,
+  setCurrentLeaderboardData,
+} from "./LeaderboardSlice";
+import { db } from "../../store/config";
+import { setCurrentSearchImageURL } from "../SearchImage/SearchImageSlice";
+import searchImages from "../SearchImage/SearchImages";
 
 export interface User {
   id?: string;
@@ -10,9 +18,80 @@ export interface User {
 }
 
 const Leaderboard = () => {
+  const dispatch = useAppDispatch();
+
   const currentLeaderboardData = useAppSelector(
     (state) => state.leaderboard.currentLeaderboardData
   );
+
+  const currentSearchImage = useAppSelector(
+    (state) => state.currentSearchImage.searchImage
+  );
+
+  const allLeaderboardData = useAppSelector(
+    (state) => state.leaderboard.allLeaderboardData
+  );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const wiiPlayersResults = collection(db, `results/wii/players`);
+
+        // order results
+        const wiiResultsQuery = query(wiiPlayersResults, orderBy("time"));
+
+        // async get data:
+        const wiiLeaderboardData = await getDocs(wiiResultsQuery);
+
+        const wiiDataArray = wiiLeaderboardData.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        const snesPlayersResults = collection(db, `results/snes/players`);
+
+        // order results
+        const snesResultsQuery = query(snesPlayersResults, orderBy("time"));
+
+        // async get data:
+        const snesLeaderboardData = await getDocs(snesResultsQuery);
+
+        const snesDataArray = snesLeaderboardData.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        const ps2PlayersResults = collection(db, `results/ps2/players`);
+
+        // order results
+        const ps2ResultsQuery = query(ps2PlayersResults, orderBy("time"));
+
+        // async get data:
+        const ps2LeaderboardData = await getDocs(ps2ResultsQuery);
+
+        const ps2DataArray = ps2LeaderboardData.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        dispatch(
+          setAllLeaderboardData([snesDataArray, wiiDataArray, ps2DataArray])
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const index = searchImages.findIndex(
+      (image) => image.name == currentSearchImage
+    );
+
+    dispatch(setCurrentLeaderboardData(allLeaderboardData[index]));
+    dispatch(setCurrentSearchImageURL(searchImages[index].url));
+  });
 
   return (
     <div>
