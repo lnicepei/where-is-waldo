e351613dc10ede59de3839c39621e86b9ce44e20
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-15 07:37:47 +0300

    Style head, rows, add font

--- src/components/Leaderboard/Leaderboard.style.ts
+++ src/components/Leaderboard/Leaderboard.style.ts
@@ -3,8 +3,20 @@ import styled from "styled-components";
 export const StyledTable = styled.table`
   width: 100%;
   table-layout: fixed;
-  
-  tr{
+  border: #ffe97c;
+
+  table,th,td {
+    border-collapse: separate;
+    border-spacing:2px;
+    border-style: solid;
+    border-width:thin;
+  }
+
+  background-color: #ffe97c;
+
+  tr {
+    font-family: 'Playfair Display', serif;
+    font-size: 1.2rem;
     text-align: center;
   }
-`
+`;
