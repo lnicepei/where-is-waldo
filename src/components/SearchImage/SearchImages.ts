3d67f4c91fe5d576ac8904055c35498e4cbd5c5c
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-15 16:26:04 +0300

    Alter relative path for images

--- src/components/SearchImage/SearchImages.ts
+++ src/components/SearchImage/SearchImages.ts
@@ -1,64 +1,64 @@
 const searchImages = [
   {
     name: "SNES",
-    url: "src/components/SearchImage/SearchImages/pierre-roussel-snes-phone2-pal.jpg",
+    url: "../where-is-waldo/SearchImages/pierre-roussel-snes-phone2-pal.jpg",
     heroes: [
       {
         name: "Red Ranger",
         found: false,
-        image: "src/components/Header/HeroImages/red-ranger-power.png",
+        image: "../HeroImages/red-ranger-power.png",
       },
       {
         name: "Donatello",
         found: false,
-        image: "src/components/Header/HeroImages/Donnyboy.webp",
+        image: "../HeroImages/Donnyboy.webp",
       },
       {
         name: "Bogyy B",
         found: false,
-        image: "src/components/Header/HeroImages/bogyy-b.png",
+        image: "../HeroImages/bogyy-b.png",
       },
     ],
   },
   {
     name: "Wii",
-    url: "src/components/SearchImage/SearchImages/wii.jpg",
+    url: "../where-is-waldo/SearchImages/wii.jpg",
     heroes: [
       {
         name: "Bowser",
         found: false,
-        image: "src/components/Header/HeroImages/Bowser.png",
+        image: "../HeroImages/Bowser.png",
       },
       {
         name: "Luigi",
         found: false,
-        image: "src/components/Header/HeroImages/Luigi.webp",
+        image: "../HeroImages/Luigi.webp",
       },
       {
         name: "Rabbid",
         found: false,
-        image: "src/components/Header/HeroImages/Rabbid.webp",
+        image: "../HeroImages/Rabbid.webp",
       },
     ],
   },
   {
     name: "PS2",
-    url: "src/components/SearchImage/SearchImages/ps2.jpg",
+    url: "../where-is-waldo/SearchImages/ps2.jpg",
     heroes: [
       {
         name: "Okami",
         found: false,
-        image: "src/components/Header/HeroImages/okami-wolf.webp",
+        image: "../HeroImages/okami-wolf.webp",
       },
       {
         name: "Legolas",
         found: false,
-        image: "src/components/Header/HeroImages/Legolas.webp",
+        image: "../HeroImages/Legolas.webp",
       },
       {
         name: "Kratos",
         found: false,
-        image: "src/components/Header/HeroImages/kratos.png",
+        image: "../HeroImages/kratos.png",
       },
     ],
   },
