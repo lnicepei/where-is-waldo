092527d059aff6a735a1993a5acabae78fc1692f
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-15 07:54:54 +0300

    Try to place it over everything

--- src/components/Timer/Timer.style.ts
+++ src/components/Timer/Timer.style.ts
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import Timer from "./Timer";
 
 export const StyledTimer = styled(Timer)`
+  display: flex;
   position: absolute;
-  top: 0;
-  color: red;
-  right: 0;
+  top: 50%;
+  justify-content: center;
+  color: white;
+  user-select: none;
+  background-color: rgba(41, 59, 72, 0.3);
+  z-index: 2;
 `;
