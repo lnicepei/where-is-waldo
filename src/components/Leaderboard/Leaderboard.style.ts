020048a6defc0b6917d681af34c80e12c2787471
lnicepei <dmitrii.shaidorov@gmail.com>
2022-09-15 13:06:29 +0300

    Set size of columns, color and font

--- src/components/Leaderboard/Leaderboard.style.ts
+++ src/components/Leaderboard/Leaderboard.style.ts
@@ -1,22 +1,47 @@
 import styled from "styled-components";
 
 export const StyledTable = styled.table`
-  width: 100%;
+  width: 50%;
   table-layout: fixed;
-  border: #ffe97c;
+  height: 300px;
+  overflow-x: auto;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  font-family: "Orbitron", serif;
+  backdrop-filter: blur(5px);
+  padding: 15px;
+  margin: 15px;
+  box-sizing: border-box;
+  text-align: center;
 
-  table,th,td {
-    border-collapse: separate;
-    border-spacing:2px;
-    border-style: solid;
-    border-width:thin;
+  th:nth-child(1) {
+    width: 50px;
   }
 
-  background-color: #ffe97c;
+  th:nth-child(3) {
+    width: 100px;
+  }
 
-  tr {
-    font-family: 'Playfair Display', serif;
-    font-size: 1.2rem;
+  th {
     text-align: center;
+    padding: 20px 15px;
+    font-weight: 500;
+    font-size: 12px;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  td {
+    padding: 15px;
+    text-align: left;
+    vertical-align: middle;
+    font-weight: 300;
+    font-size: 12px;
+    color: #fff;
+    border-bottom: solid 1px rgba(255, 255, 255, 0.1);
+    text-align: center;
+  }
+
+  @media screen and (max-width: 780px) {
+    width: 80%;
   }
 `;
